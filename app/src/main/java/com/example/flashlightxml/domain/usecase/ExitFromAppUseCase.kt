370a4e0ef70249c71e9da3e5235b862ee0068a1a
nife <nife0612@gmail.com>
2023-08-22 14:02:26 +0400

    It has finished ExitUseCase

--- app/src/main/java/com/example/flashlightxml/domain/usecase/ExitFromAppUseCase.kt
+++ app/src/main/java/com/example/flashlightxml/domain/usecase/ExitFromAppUseCase.kt
@@ -1,10 +1,15 @@
 package com.example.flashlightxml.domain.usecase
 
+import kotlin.system.exitProcess
+
 // Класс для выхода из приложения
 
 // Архетектура https://www.youtube.com/playlist?list=PLeF3l86ZMVkLQbdRL6Ra4cr_cmPROj94y
 class ExitFromAppUseCase {
 
-    public fun execute(){}
+    fun execute(){
+        // Закрыть приложение
+        exitProcess(0)
+    }
 
 }
--- app/src/main/java/com/example/flashlightxml/presentanion/MainActivity.kt
+++ app/src/main/java/com/example/flashlightxml/presentanion/MainActivity.kt
@@ -2,16 +2,27 @@ package com.example.flashlightxml.presentanion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.flashlightxml.R
+import com.example.flashlightxml.databinding.ActivityMainBinding
+import com.example.flashlightxml.domain.usecase.ExitFromAppUseCase
 
 class MainActivity : AppCompatActivity() {
 
-    // Будем использовать ViewBinding?
+    // Создаю ViewBinding (https://developer.android.com/topic/libraries/view-binding)
+    private lateinit var binding : ActivityMainBinding
+
+    private val exitFromAppUseCase = ExitFromAppUseCase()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        // Создаю ViewBinding (https://developer.android.com/topic/libraries/view-binding)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        // ViewBinding вместо findViewByID
+        binding.exitButton.setOnClickListener{
+            exitFromAppUseCase.execute()
+        }
     }
 
 
