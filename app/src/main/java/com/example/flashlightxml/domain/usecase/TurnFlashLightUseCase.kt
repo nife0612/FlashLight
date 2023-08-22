cedeb26c7fb585525d46bb720c892097a17f3218
HungryChaika <vologzhanin_00@mail.ru>
2023-08-22 19:42:15 +0400

    Flashlight off use case

--- app/src/main/java/com/example/flashlightxml/domain/usecase/TurnFlashLightUseCase.kt
+++ app/src/main/java/com/example/flashlightxml/domain/usecase/TurnFlashLightUseCase.kt
@@ -4,25 +4,38 @@ package com.example.flashlightxml.domain.usecase
 import android.hardware.camera2.CameraAccessException
 import android.os.Build
 import android.util.Log
-import androidx.annotation.RequiresApi
 import com.example.flashlightxml.data.Data
-
 
 // Класс для включения/выключения фонарика
 
 // Возможно стоит разделить включение и выключение на разные классы
 class TurnFlashLightUseCase(private val data: Data) {
+    private var lightFlag: Boolean = false
 
     private fun lightOn() {
         try {
             data.manager.setTorchMode(data.manager.cameraIdList[0], true)
+            lightFlag = true
         } catch (e: CameraAccessException) {
             Log.e("CameraErrror", e.toString())
         }
     }
 
+    private fun lightOff() {
+        try {
+            data.manager.setTorchMode(data.manager.cameraIdList[0], false)
+            lightFlag = false
+        } catch (e: CameraAccessException) {
+            Log.e("CameraError", e.toString())
+        }
+    }
+
     public fun execute(){
-        lightOn()
+        if(lightFlag) {
+            lightOff()
+        } else {
+            lightOn()
+        }
     }
 
 }
