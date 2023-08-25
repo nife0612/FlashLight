bfabb6f22a6f21ac9835f2f4b6bdd94f9c81f8c5
HungryChaika <vologzhanin_00@mail.ru>
2023-08-25 15:19:28 +0400

    working SosUseCase

--- app/src/main/java/com/example/flashlightxml/domain/usecase/SOSUseCase.kt
+++ app/src/main/java/com/example/flashlightxml/domain/usecase/SOSUseCase.kt
@@ -2,28 +2,32 @@ package com.example.flashlightxml.domain.usecase
 
 import com.example.flashlightxml.data.Data
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 // Класс для сигнала SOS
 class SOSUseCase(private val data: Data) {
 
     private val numOfFlashesInSignal = 3
 
-    private fun sosSignal() {
+    private suspend fun sosSignal() {
         flash(500L)
-        //delay(100L)
+        delay(100L)
         flash(1000L)
-        //delay(100L)
+        delay(100L)
         flash(500L)
     }
 
-    private fun flash(timeMillis: Long) {
+    private suspend fun flash(timeMillis: Long) {
         data.manager.setTorchMode(data.manager.cameraIdList[0], true)
-        //delay(timeMillis)
+        delay(timeMillis)
         data.manager.setTorchMode(data.manager.cameraIdList[0], false)
     }
 
-    public fun execute(){
-        sosSignal()
+    public fun execute() = runBlocking {
+        launch {
+            sosSignal()
+        }
     }
 
 }
