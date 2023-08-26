26eb34ce23f9d466882e4641e0e5aeebf85a7b45
nife <nife0612@gmail.com>
2023-08-26 10:55:45 +0400

    TimeDelay was create

---
+++ app/src/main/java/com/example/flashlightxml/domain/model/TimeDelay.kt
@@ -0,0 +1,7 @@
+package com.example.flashlightxml.domain.model
+
+object TimeDelay{
+    const val WAITING = 100L
+    const val SHORT = 500L
+    const val Long = 1000L
+}
--- app/src/main/java/com/example/flashlightxml/domain/usecase/SOSUseCase.kt
+++ app/src/main/java/com/example/flashlightxml/domain/usecase/SOSUseCase.kt
@@ -1,11 +1,16 @@
 package com.example.flashlightxml.domain.usecase
 
-import android.content.Context
-import android.hardware.camera2.CameraManager
+
 import com.example.flashlightxml.domain.repository.FlashLightRepository
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+
+import com.example.flashlightxml.domain.model.TimeDelay.SHORT
+import com.example.flashlightxml.domain.model.TimeDelay.WAITING
+import com.example.flashlightxml.domain.model.TimeDelay.Long
+
+
 
 // Класс для сигнала SOS
 class SOSUseCase(private val repository: FlashLightRepository) {
@@ -14,12 +19,13 @@ class SOSUseCase(private val repository: FlashLightRepository) {
     private val numOfFlashesInSignal = 3
 
     private suspend fun sosSignal() {
-        flash(500L)
-        delay(100L)
-        flash(1000L)
-        delay(100L)
-        flash(500L)
+        flash(WAITING)
+        delay(SHORT)
+        flash(Long)
+        delay(SHORT)
+        flash(SHORT)
     }
+
 
     private suspend fun flash(timeMillis: Long) {
         camManager.setTorchMode((camManager.cameraIdList[0]), true)
