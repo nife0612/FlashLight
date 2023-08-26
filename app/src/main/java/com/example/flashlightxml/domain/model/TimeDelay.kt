a65f3cfbb7d89d731e760068f67606b0ec60b3f2
HungryChaika <vologzhanin_00@mail.ru>
2023-08-26 11:27:39 +0400

    Add waiting consts and SosUseCase had finished

--- app/src/main/java/com/example/flashlightxml/domain/model/TimeDelay.kt
+++
@@ -1,7 +0,0 @@
-package com.example.flashlightxml.domain.model
-
-object TimeDelay{
-    const val WAITING = 100L
-    const val SHORT = 500L
-    const val Long = 1000L
-}
--- app/src/main/java/com/example/flashlightxml/domain/usecase/SOSUseCase.kt
+++ app/src/main/java/com/example/flashlightxml/domain/usecase/SOSUseCase.kt
@@ -6,26 +6,31 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-import com.example.flashlightxml.domain.model.TimeDelay.SHORT
-import com.example.flashlightxml.domain.model.TimeDelay.WAITING
-import com.example.flashlightxml.domain.model.TimeDelay.Long
-
-
-
 // Класс для сигнала SOS
 class SOSUseCase(private val repository: FlashLightRepository) {
 
     private val camManager= repository.get().camManager
-    private val numOfFlashesInSignal = 3
 
-    private suspend fun sosSignal() {
-        flash(WAITING)
-        delay(SHORT)
-        flash(Long)
-        delay(SHORT)
-        flash(SHORT)
+    companion object {
+        private const val COUNT_OF_FLASHES = 9
+        private const val WAITING = 100L
+        private const val SHORT = 500L
+        private const val LONG = 1000L
     }
 
+    private suspend fun sosSignal() {
+        var counter = 1
+        var isLongFlash = false
+        while(counter <= COUNT_OF_FLASHES) {
+            when(isLongFlash) {
+                true -> flash(LONG)
+                false -> flash(SHORT)
+            }
+            delay(WAITING)
+            if(counter % 3 == 0) isLongFlash = !isLongFlash
+            counter++
+        }
+    }
 
     private suspend fun flash(timeMillis: Long) {
         camManager.setTorchMode((camManager.cameraIdList[0]), true)
